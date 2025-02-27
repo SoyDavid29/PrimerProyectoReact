a4ca65aafbf296b6f762ebf8935083fde1a82b9b
DavidRodriguez <studiandoprogramacion@gmail.com>
2025-02-26 19:04:36 -0600

    trabajando con los useState

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import { use } from "react"
 import { db } from "./data/db"
 
 function App() {
-  const [data, setData] = useState(db) /* creamos el useState con el nombre data y la funcion de nombre setData que retorna lo que hay en nuestra base de datos db */
+  const [data, setData] = useState(db) /* creamos el useState (objeto) con el nombre data y la funcion de nombre setData que retorna lo que hay en nuestra base de datos db */
+  const [cart, setCart] = useState([]) 
   return (
     <>
       <main className="container-xl mt-5">
@@ -12,7 +13,7 @@ function App() {
 
         <div className="row mt-5">
           {data.map((guitar) => (  /* nombramos a cada objeto de la coleccioon de datos (guitar) y accedemos.
-                                       iteramos sobre data y map nos ayuda a generar un nuevo arregoo*/
+                                       iteramos sobre data y map nos ayuda a generar un nuevo arreglo*/
             <Guitar /* componente */
               key={guitar.id} /* prop especial que se utiliza en componentes que utilizan .map para iterar */
               guitar={guitar} /* prop de nombre guitar, que accede a nuestro objeto {guitar} */
--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -1,6 +1,10 @@
 export default function Guitar({guitar}) { /* accedemos al prop de guitarra que creamos */
 
     const {id, name, image, description, price} = guitar /* aplicamos distructuring al objeto de guitar para acceder a sus datos */
+  
+    const handleClick = (id) => { /* Declaramos funcion */
+        console.log("diste clic wey", id)
+    }
     return (
         <div className="col-md-6 col-lg-4 my-4 row align-items-center">
             <div className="col-4">
@@ -13,6 +17,7 @@ export default function Guitar({guitar}) { /* accedemos al prop de guitarra que 
                 <button
                     type="button"
                     className="btn btn-dark w-100"
+                    onClick={() => handleClick(guitar)}   /* agregamos el evento de click al button de este componente */
                 >Agregar al Carrito</button>
             </div>
         </div>
